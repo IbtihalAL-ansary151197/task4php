066fd4e64bd3a2cb23185442f5203c5342b00aa3
IbtihalAL-ansary151197 <81520632+IbtihalAL-ansary151197@users.noreply.github.com>
2021-10-07 08:54:47 +0200

    Add files via upload

---
+++ index2.php
@@ -0,0 +1,64 @@
+<?php 
+
+function clean($input){
+
+    $input = stripslashes($input);
+    $input = htmlspecialchars($input);
+    $input = trim($input);
+
+    return $input;
+
+}
+
+
+   
+function validate($input,$flag,$length = 6){
+   
+    $status = true;
+
+    switch ($flag) {
+        case 1:
+            # code...
+            if(empty($input)){
+                $status = false;
+            }
+            break;
+
+        case 2: 
+            # code ... 
+             if(!filter_var($input,FILTER_VALIDATE_EMAIL)){
+                $status = false;
+             }
+            break;
+        
+        case 3: 
+            #code ... 
+            if(strlen($input) < $length){
+                $status = false;
+            }    
+            break;
+
+        case 4: 
+            #code ... 
+            if(!filter_var($input,FILTER_VALIDATE_URL)){
+                $status = false;
+            }    
+            break;
+
+            case 5: 
+                #code ... 
+                if(strlen($input) < $length){
+                    $status = false;
+                }    
+                break;
+    }
+    return $status;
+
+}
+
+
+
+
+
+
+?>
